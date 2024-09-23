815a8576da91b3c1f955eac858456b5d702ba4e1
Luis Angel Salas Laza <angel1.lasl46@gmail.com>
2024-09-23 15:26:24 -0400

    rere

--- src/main.js
+++ src/main.js
@@ -1,7 +1,34 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import TableComponent from './components/TableComponent.vue'
 import './registerServiceWorker'
 import router from './router'
 import store from './store'
+import 'bootstrap'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap-icons/font/bootstrap-icons.css';  // Asegúrate de esta línea
+import '@/assets/lib/fontawesome-v6.5.2/css/all.css'
+import '@/assets/lib/fontawesome-v6.5.2/css/sharp-light.css'
+import '@/assets/lib/fontawesome-v6.5.2/css/sharp-regular.css'
+import '@/assets/lib/fontawesome-v6.5.2/css/sharp-solid.css'
+import '@/assets/lib/fontawesome-v6.5.2/css/sharp-thin.css'
 
-createApp(App).use(store).use(router).mount('#app')
+// Importa CSS
+
+//PERSONALIZADO ASIDE
+import '@/assets/css/main.css'
+import '@/assets/css/aside.css'
+import 'sweetalert2';
+import 'sweetalert2/dist/sweetalert2.min.css';
+import '@/assets/js/validation.js'
+import '@/assets/js/alerts.js'
+import '@/assets/js/aside.js'
+
+const app = createApp(App);
+
+// Usa el store y el router
+app.use(store);
+app.use(router);
+
+// Monta la aplicación
+app.mount('#app');
--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,18 @@
 const { defineConfig } = require('@vue/cli-service')
+const webpack = require('webpack')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+
+  devServer: {
+    port: 9090, // Configura el puerto a 80
+    host: '0.0.0.0', // Permite conexiones desde cualquier IP
+    https: false, // Cambia a true si necesitas HTTPS
+  },
+  configureWebpack: {
+    plugins:[
+      new webpack.DefinePlugin({
+        '__VUE_PROD_HYDRATION_MISMATCH_DETAILS__': JSON.stringify(false),
+      })
+    ]
+  }
 })
